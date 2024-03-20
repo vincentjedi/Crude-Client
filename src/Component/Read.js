889761a1f8a1ff634cface705f6752cc6851a256
vincentjedi <kitivincent@gmail.com>
2024-03-20 19:11:21 +0300

    second commit

--- src/Component/Read.js
+++ src/Component/Read.js
@@ -16,7 +16,7 @@ axios.get('http://localhost:8081/read/'+id)
 setCustomer(res.data[0])
 })
 .catch(err => console.log(err))
-    },[])
+    },[id])
 
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
